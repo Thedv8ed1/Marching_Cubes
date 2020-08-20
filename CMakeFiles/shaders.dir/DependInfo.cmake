0f82d43a85fd53e1b2c364d48eae907351c72a90
Jason Sadler <thedv8ed1@hotmail.com>
2020-08-19 17:43:38 -0700

    added all files

---
+++ CMakeFiles/shaders.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/thedv/Desktop/lab-04-files/application.cpp" "/mnt/c/Users/thedv/Desktop/lab-04-files/CMakeFiles/shaders.dir/application.cpp.o"
+  "/mnt/c/Users/thedv/Desktop/lab-04-files/gl_arcball.cpp" "/mnt/c/Users/thedv/Desktop/lab-04-files/CMakeFiles/shaders.dir/gl_arcball.cpp.o"
+  "/mnt/c/Users/thedv/Desktop/lab-04-files/gl_viewer.cpp" "/mnt/c/Users/thedv/Desktop/lab-04-files/CMakeFiles/shaders.dir/gl_viewer.cpp.o"
+  "/mnt/c/Users/thedv/Desktop/lab-04-files/main.cpp" "/mnt/c/Users/thedv/Desktop/lab-04-files/CMakeFiles/shaders.dir/main.cpp.o"
+  "/mnt/c/Users/thedv/Desktop/lab-04-files/obj.cpp" "/mnt/c/Users/thedv/Desktop/lab-04-files/CMakeFiles/shaders.dir/obj.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/shaders.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shaders.dir/application.cpp.o"
+  "CMakeFiles/shaders.dir/gl_arcball.cpp.o"
+  "CMakeFiles/shaders.dir/gl_viewer.cpp.o"
+  "CMakeFiles/shaders.dir/main.cpp.o"
+  "CMakeFiles/shaders.dir/obj.cpp.o"
+  "shaders"
+  "shaders.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
